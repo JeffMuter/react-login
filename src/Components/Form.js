b00f0c4d01355cdabdef23c89c9f6dbcf6287e63
JeffMuter <muterjeffery@gmail.com>
2023-03-11 12:30:30 -0500

    checkbox edited and styling for sign-up button

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,63 +3,25 @@ import PasswordChecklist from "react-password-checklist";
 import { useState } from "react";
 
 const Form = () => {
-  const [password, setPassword] = useState("");
-  const [passwordAgain, setPasswordAgain] = useState("");
-
-  let checkVis = false;
-
-  const showChecks = () => {
-    if (checkVis) {
-      return (
-        <PasswordChecklist
-          rules={["minLength", "specialChar", "number", "capital", "match"]}
-          minLength={5}
-          value={password}
-          valueAgain={passwordAgain}
-          onChange={(isValid) => {}}
-        ></PasswordChecklist>
-      );
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const clicked = () => {
-    checkVis = true;
-  };
-
   return (
-    <form className={classes.formBox} onSubmit={handleSubmit} onClick={clicked}>
+    <form className={classes.formBox}>
       <div>
         <h2 className={classes.formTitle}>Create Account</h2>
       </div>
       <div className={classes.inputBoxes}>
         <input type="text" placeholder="Username"></input> <br />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>{" "}
-        <br />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          onChange={(e) => setPasswordAgain(e.target.value)}
-        ></input>
+        <input type="password" placeholder="Password"></input> <br />
+        <input type="password" placeholder="Confirm Password"></input>
         <br />
       </div>
-      <div>{showChecks}</div>
       <div className={classes.termsCheckbox}>
-        <input type="checkbox" />
-        <label>
-          {" "}
-          I agree to the <a href="#">Terms & Conditions.</a>
+        <label className={classes.termsLabel}>
+          <input type="checkbox" />
+          <span></span>I agree to the <a href="#">Terms & Conditions.</a>
         </label>
       </div>
       <div>
-        <button className={classes.signUpButton} type="button">
+        <button className={classes.signUpButton} type="submit">
           Sign Up
         </button>
       </div>
